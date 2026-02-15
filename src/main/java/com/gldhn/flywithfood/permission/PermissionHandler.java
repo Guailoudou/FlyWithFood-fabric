432628b90ee3523a50caba57e54e7fa64b977817
Guailoudou <guailoudou@outlook.com>
2026-02-15 14:05:09 +0800

    使用 Fabric Permissions API 重写了权限管理系统

--- src/main/java/com/gldhn/flywithfood/permission/PermissionHandler.java
+++
@@ -1,7 +0,0 @@
-package com.gldhn.flywithfood.permission;
-
-import net.minecraft.server.level.ServerPlayer;
-
-public interface PermissionHandler {
-    boolean hasPermission(ServerPlayer player, String permission, int defaultOpLevel);
-}
